a93c6faa6e5843dd579bc009c07678671590c5bb
jbryer <jason@bryer.org>
2022-07-26 16:42:29 -0400

    Fixed copying of files in the right order.

--- start/run-daacs-web.sh
+++ start/run-daacs-web.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+figlet -t -k -f /usr/share/figlet/small.flf "Updating Web config"
+cp /usr/local/daacs/DAACS-Web/node/package.json /daacs/start/package.json
+# Update the environment.js file. Add the IP address and domain to the hostWhitelist parameter
+sudo rm /usr/local/daacs/DAACS-Web/daacs/config/web-config.json
+# sudo -c 'echo /usr/local/daacs/DAACS-Web/daacs/config/environment.js > /usr/local/daacs/DAACS-Web/daacs/config/web-config.json'
+cp /usr/local/daacs/DAACS-Web/daacs/config/environment.js /usr/local/daacs/DAACS-Web/daacs/config/web-config.json
+
 figlet -t -k -f /usr/share/figlet/small.flf "Starting DAACS-Web"
 PORT=8000 CLIENT_CONFIG_PATH=/usr/local/daacs/DAACS-Web/daacs/config/web-config.json npm start --max-old-space-size=1024
 
-figlet -t -k -f /usr/share/figlet/small.flf "Updating Web config"
-# Update the environment.js file. Add the IP address and domain to the hostWhitelist parameter
-sudo rm /usr/local/daacs/DAACS-Web/daacs/config/web-config.json
-sudo -c 'echo /usr/local/daacs/DAACS-Web/daacs/config/environment.js > /usr/local/daacs/DAACS-Web/daacs/config/web-config.json'
